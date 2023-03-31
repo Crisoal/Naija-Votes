41c618e8dbb5228f19dfdec624150f71207e2a2b
Christiana Ola <102651806+Crisoal@users.noreply.github.com>
2023-03-31 13:08:08 +0400

    Update header.php

--- voters/inc/header.php
+++ voters/inc/header.php
@@ -1,4 +1,4 @@
-<!-- <?php 
+ <?php 
     session_start();
     require_once("../admin/inc/config.php");
 
@@ -7,7 +7,7 @@
         echo "<script> location.assign('../admin/inc/logout.php'); </script>";
         die;
     }
-?> -->
+?> 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -39,7 +39,26 @@
             </div>
         </div>
 
- 
+        <div class="row bg-green">
+    <div class="col-12 bg-green">
+    <nav class="navbar navbar-expand-lg navbar-light bg-green">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link text-white mr-3" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white mr-3" href="../admin/logout.php">Logout</a>
+        </li>
+         </ul>
+    </div>
+    </nav>
+</div>
+</div>
 
 
 
